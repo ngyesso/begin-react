bdf2df7caedf36715379d45ab7aa358a11eb6566
ngyesso <ngyesso@gmail.com>
2021-09-08 21:44:00 +0900

    :art: useRef 적용

--- src/InputSample.jsx
+++ src/InputSample.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function InputSample() {
   const [inputs, setInputs] = useState({
     name: "",
     nickname: "",
   });
+  const nameInput = useRef();
 
   const { name, nickname } = inputs;
 
@@ -27,11 +28,18 @@ function InputSample() {
       name: "",
       nickname: "",
     });
+    nameInput.current.focus();
   };
 
   return (
     <>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
       <input
         name="nickname"
         placeholder="닉네임"
@@ -41,7 +49,7 @@ function InputSample() {
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값: </b>
-        {name} ({nickname}
+        {name} ({nickname})
       </div>
     </>
   );
