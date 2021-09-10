e8ca6fd743f7819fbad24ab68f304a7683cdaa36
ngyesso <ngyesso@gmail.com>
2021-09-10 19:33:53 +0900

    :zap: array 항목 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,68 @@
 import "./App.css";
+import { useRef, useState } from "react";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  return <UserList />;
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // 불변성을 지키면서 배열에 새 항목을 추가하는 방법
+    // setUsers([...users, user]); // 방법 1) spread 연산자 사용
+    setUsers(users.concat(user)); // 방법 2) concat 사용
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
